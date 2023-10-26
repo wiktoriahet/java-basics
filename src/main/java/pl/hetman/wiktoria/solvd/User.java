b5cfae8b1d98c29266a888a0051a34d3c3355b0a
wiktoriahet <wiktoriaspyc@gmail.com>
2023-10-26 14:55:59 +0200

    created simple output method in main to print parameters given in cmd

--- src/main/java/pl/hetman/wiktoria/solvd/User.java
+++ src/main/java/pl/hetman/wiktoria/solvd/User.java
@@ -2,6 +2,8 @@ package pl.hetman.wiktoria.solvd;
 
 public class User {
     public static void main(String[] args) {
-        System.out.println(args[0]);
+        for(int i = 0; i<args.length; i++){
+            System.out.println(args[i]);
+        }
     }
 }
