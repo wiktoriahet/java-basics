acb5260a3bf7506685d19f8fa4c6ccc7d7bc115d
wiktoriahet <wiktoriaspyc@gmail.com>
2023-10-26 11:52:28 +0200

    created bubble sort example

---
+++ src/main/java/pl/hetman/wiktoria/solvd/bubblesort/BubbleSort.java
@@ -0,0 +1,21 @@
+package pl.hetman.wiktoria.solvd.bubblesort;
+
+import java.util.stream.IntStream;
+
+public class BubbleSort {
+
+    public int[] bubbleSort(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
